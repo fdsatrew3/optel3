8b2187d0874fe2dcd92366c199d793d0e631518f
Александр Плеханов <Plehalex09@gmail.com>
2021-03-04 15:15:22 +0300

    [OPT-1] - Optimization algorithms project was created

---
+++ Optimization.Algorithms/Core/IOptimizationAlgorithmDecisions.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Optimization.Algorithms.Core
+{
+    public interface IOptimizationAlgorithmDecisions<T> : IOptimizationAlgorithm<List<T>>
+    {
+
+    }
+}
