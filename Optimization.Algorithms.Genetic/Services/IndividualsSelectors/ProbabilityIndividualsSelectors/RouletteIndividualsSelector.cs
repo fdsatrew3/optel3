cf65e107fcca236b26f621b85485ff069dfe1d26
Александр Плеханов <Plehalex09@gmail.com>
2021-03-09 22:27:51 +0300

    [OPT-2][OPT-7] - Individual selectors were added to new genetic algorithm project

---
+++ Optimization.Algorithms.Genetic/Services/IndividualsSelectors/ProbabilityIndividualsSelectors/RouletteIndividualsSelector.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Optimization.Algorithms.Utilities.Probabilities;
+
+using Optimization.Algorithms.Genetic.Data;
+
+namespace Optimization.Algorithms.Genetic.Services.IndividualsSelectors.ProbabilityIndividualsSelectors
+{
+    public class RouletteIndividualsSelector<I> : ProbabilityIndividualsSelector<I>
+        where I : ICalculatedIndividual
+    {
+        public RouletteIndividualsSelector(RandomProbabilityChecker randomProbabilityChecker, int selectionCount)
+            : base(randomProbabilityChecker, selectionCount)
+        {
+
+        }
+
+        /// <summary>
+        /// Calculate probabilities for individuals
+        /// </summary>
+        /// <param name="individuals">Collection of individuals</param>
+        /// <returns>Individuals with probabilities</returns>
+        protected override IEnumerable<ItemProbability<I>> CalculateProbabilities(ICollection<I> individualsCollection)
+        {
+            var sumFitness = individualsCollection.Sum(x => x.FitnessFunctionValue);
+
+            foreach (var individual in individualsCollection)
+            {
+                var probability = individual.FitnessFunctionValue / sumFitness;
+
+                yield return new ItemProbability<I>(individual, probability);
+            }
+        }
+    }
+}
