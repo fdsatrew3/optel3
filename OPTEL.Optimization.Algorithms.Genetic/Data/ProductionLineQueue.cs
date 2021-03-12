bbe9687303e8e4bbda82b5e5a2a835644ff68676
Александр Плеханов <Plehalex09@gmail.com>
2021-03-12 21:31:46 +0300

    [OPT-2][OPT-8] - Data for adoptation of genetic algorithms added

---
+++ OPTEL.Optimization.Algorithms.Genetic/Data/ProductionLineQueue.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using OPTEL.Data;
+
+namespace OPTEL.Optimization.Algorithms.Genetic.Data
+{
+    public class ProductionLineQueue
+    {
+        public Extruder Extruder { get; set; }
+
+        public List<Order> Orders { get; set; }
+    }
+}
---
+++ OPTEL.Optimization.Algorithms.Genetic/Data/ProductionPlan.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace OPTEL.Optimization.Algorithms.Genetic.Data
+{
+    public class ProductionPlan
+    {
+        public List<ProductionLineQueue> ProductionLineQueues { get; set; }
+    }
+}
