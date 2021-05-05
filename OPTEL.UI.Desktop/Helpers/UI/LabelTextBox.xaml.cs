eeeb08c737eb2085f40d9321930ed2e6bfde305b
fdsatrew <fdsatrewq3@gmail.com>
2021-05-05 22:51:17 +0300

    Added label textbox control

---
+++ OPTEL.UI.Desktop/Helpers/UI/LabelTextBox.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Controls;
+
+namespace OPTEL.UI.Desktop.Helpers.UI
+{
+    /// <summary>
+    /// Логика взаимодействия для LabelTextBox.xaml
+    /// </summary>
+    public partial class LabelTextBox : UserControl
+    {
+        public LabelTextBox()
+        {
+            InitializeComponent();
+        }
+
+        public string Title
+        {
+            get { return BaseLabel.Content.ToString(); }
+            set
+            {
+                BaseLabel.Content = value;
+            }
+        }
+
+        public string Text
+        {
+            get { return BaseTextBox.Text; }
+            set
+            {
+                BaseTextBox.Text = value;
+            }
+        }
+    }
+}
