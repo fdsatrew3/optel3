33b8f2b6df94fd0326069c900c9b1f102a93790c
fdsatrew <fdsatrewq3@gmail.com>
2021-05-06 23:14:05 +0300

    fix

--- OPTEL.UI.Desktop/ViewModels/ExtruderCalibrationsViewModel.cs
+++ OPTEL.UI.Desktop/ViewModels/ExtruderCalibrationsViewModel.cs
@@ -1,20 +1,43 @@
 ﻿using OPTEL.Data;
+using OPTEL.UI.Desktop.Helpers;
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
+using System.Linq;
 
 namespace OPTEL.UI.Desktop.ViewModels
 {
     public class ExtruderCalibrationsViewModel
     {
-        private CalibrationChange _selectedExtruderCalibration;
-
+        #region Properties
         public CalibrationChange SelectedExtruderCalibration { get => _selectedExtruderCalibration; set => _selectedExtruderCalibration = value; }
-
         public IEnumerable<CalibrationChange> ExtruderCalibrations { get; set; }
+        #endregion
+        #region Fields
+        private CalibrationChange _selectedExtruderCalibration;
+        #endregion
+        #region Commands
+        private RelayCommand _selectFirstListBoxEntryIfExists;
 
+        public RelayCommand SelectFirstListBoxEntryIfExists
+        {
+            get
+            {
+                return _selectFirstListBoxEntryIfExists ??= new RelayCommand(obj =>
+                {
+                    CalibrationChange calibrationChange;
+                    try
+                    {
+                        calibrationChange = ExtruderCalibrations.First();
+                    }
+                    catch
+                    {
+                        return;
+                    }
+                    SelectedExtruderCalibration = calibrationChange;
+                });
+            }
+        }
+        #endregion
         public ExtruderCalibrationsViewModel()
         {
             ExtruderCalibrations = Database.instance.CalibrationChangeRepository.GetAll();
