c5e14b989b1b0b139bbc5ce6506646369ba99f37
fdsatrew <fdsatrewq3@gmail.com>
2021-05-11 03:22:37 +0300

    Fixed gantt chart

--- OPTEL.UI.Desktop/Services/GanttChartManagers/MainWindowGanttChartManagerService.cs
+++ OPTEL.UI.Desktop/Services/GanttChartManagers/MainWindowGanttChartManagerService.cs
@@ -44,21 +44,24 @@ namespace OPTEL.UI.Desktop.Services.GanttChartManagers
             TimeSpan currentOrderExecutionTime, currentOrderReconfigurationTime;
             Task lastOrderTask;
             Order lastOrder;
-            Debug.WriteLine("=========================================== 2 NOCHI BLYAT");
-            Debug.WriteLine("Time of Travel: {0:dd\\.hh\\:mm\\:ss} days", productionPlanDuration);
+            var orderExecutionTimeCalculator = new OrderExcecutionTimeCalculator();
+            var orderReconfigurationTimeCalculator = new OrdersReconfigurationTimeCalculator();
+            var executionTimeCalculator = new ExecutionTimeCalculator(orderExecutionTimeCalculator);
+            var reconfigurationTimeCalculator = new ReconfigurationTimeCalculator(orderReconfigurationTimeCalculator);
+            var productionLineQueueTimeCalculator = new ProductionLineQueueTimeCalculator(executionTimeCalculator, reconfigurationTimeCalculator);
             foreach (ProductionLineQueue queue in plan.ProductionLineQueues)
             {
                 Task productionLineTask = new Task();
                 productionLineTask.Name = queue.ProductionLine.Name;
+                _projectManager.Add(productionLineTask);
                 _projectManager.SetStart(productionLineTask, TimeSpan.FromHours(0));
                 _projectManager.SetDuration(productionLineTask, productionPlanDuration);
-                _projectManager.SetEnd(productionLineTask, productionPlanDuration);
-                /*lastOrderTask = productionLineTask;
+                lastOrderTask = productionLineTask;
                 currentTimeSpanOffset = TimeSpan.FromSeconds(0);
                 lastOrder = null;
                 foreach (Order order in queue.Orders)
                 {
-                    if(lastOrder == null)
+                    if (lastOrder == null)
                     {
                         lastOrder = order;
                     }
@@ -66,76 +69,16 @@ namespace OPTEL.UI.Desktop.Services.GanttChartManagers
                     orderTask.Name = order.OrderNumber;
                     currentOrderExecutionTime = TimeSpan.FromMinutes(_orderExecutionTimeCalculator.Calculate(order));
                     currentOrderReconfigurationTime = TimeSpan.FromMinutes(_ordersReconfigurationTimeCalculator.Calculate(queue.ProductionLine, lastOrder, order));
-                    Debug.WriteLine(currentOrderExecutionTime.ToString());
                     currentTimeSpanOffset += currentOrderExecutionTime + currentOrderReconfigurationTime;
+                    _projectManager.Add(orderTask);
                     _projectManager.SetStart(orderTask, currentTimeSpanOffset);
-                    //_projectManager.SetDuration(orderTask, currentOrderExecutionTime);
                     _projectManager.SetDuration(orderTask, TimeSpan.FromHours(8));
                     _projectManager.SetComplete(orderTask, 1.0f);
                     _projectManager.Group(productionLineTask, orderTask);
                     _projectManager.Relate(lastOrderTask, orderTask);
                     lastOrderTask = orderTask;
-                } */
+                }
             }
-            /*
-            ProjectManager _mManager = null;
-            _mManager = new ProjectManager();
-            var work = new MyTask(_mManager) { Name = "Prepare for Work" };
-            var wake = new MyTask(_mManager) { Name = "Wake Up" };
-            var teeth = new MyTask(_mManager) { Name = "Brush Teeth" };
-            var shower = new MyTask(_mManager) { Name = "Shower" };
-            var clothes = new MyTask(_mManager) { Name = "Change into New Clothes" };
-            var hair = new MyTask(_mManager) { Name = "Blow My Hair" };
-            var pack = new MyTask(_mManager) { Name = "Pack the Suitcase" };
-
-            _mManager.Add(work);
-            _mManager.Add(wake);
-            _mManager.Add(teeth);
-            _mManager.Add(shower);
-            _mManager.Add(clothes);
-            _mManager.Add(hair);
-            _mManager.Add(pack);
-
-            // Create another 1000 tasks for stress testing
-            Random rand = new Random();
-            for (int i = 0; i < 1000; i++)
-            {
-                var task = new MyTask(_mManager) { Name = string.Format("New Task {0}", i.ToString()) };
-                _mManager.Add(task);
-                _mManager.SetStart(task, TimeSpan.FromDays(rand.Next(300)));
-                _mManager.SetDuration(task, TimeSpan.FromDays(rand.Next(50)));
-            }
-
-            // Set task durations, e.g. using ProjectManager methods 
-            _mManager.SetDuration(wake, TimeSpan.FromDays(3));
-            _mManager.SetDuration(teeth, TimeSpan.FromDays(5));
-            _mManager.SetDuration(shower, TimeSpan.FromDays(7));
-            _mManager.SetDuration(clothes, TimeSpan.FromDays(4));
-            _mManager.SetDuration(hair, TimeSpan.FromDays(3));
-            _mManager.SetDuration(pack, TimeSpan.FromDays(5));
-
-            // demostrate splitting a task
-            // Set task complete status, e.g. using newly created properties
-            wake.Complete = 0.9f;
-            teeth.Complete = 0.5f;
-            shower.Complete = 0.4f;
-
-            // Give the Tasks some organisation, setting group and precedents
-            _mManager.Group(work, wake);
-            _mManager.Group(work, teeth);
-            _mManager.Group(work, shower);
-            _mManager.Group(work, clothes);
-            _mManager.Group(work, hair);
-            _mManager.Group(work, pack);
-            _mManager.Relate(wake, teeth);
-            _mManager.Relate(wake, shower);
-            _mManager.Relate(shower, clothes);
-            _mManager.Relate(shower, hair);
-            _mManager.Relate(hair, pack);
-            _mManager.Relate(clothes, pack);
-
-            // Initialize the Chart with our ProjectManager and CreateTaskDelegate
-            _projectManager = _mManager; */
         }
 
         public void UpdateChart()
