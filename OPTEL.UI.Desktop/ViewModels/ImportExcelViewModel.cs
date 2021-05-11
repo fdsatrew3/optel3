9d18dc436e3400b45fc507517f4579da6c22ea44
fdsatrew <fdsatrewq3@gmail.com>
2021-05-11 05:17:27 +0300

    Working delete all data checkbox in excel import window

--- OPTEL.UI.Desktop/ViewModels/ImportExcelViewModel.cs
+++ OPTEL.UI.Desktop/ViewModels/ImportExcelViewModel.cs
@@ -23,6 +23,7 @@ namespace OPTEL.UI.Desktop.ViewModels
         private bool? _isDeleteAllCheckBoxChecked;
 
         private string _selectedExcelFile;
+        private string _progressBarText;
 
         private IOpenFileDialogService _openFileDialogService;
 
@@ -55,6 +56,15 @@ namespace OPTEL.UI.Desktop.ViewModels
                 OnPropertyChanged("SelectedExcelFile");
             }
         }
+        public string ProgressBarText
+        {
+            get => _progressBarText;
+            set
+            {
+                _progressBarText = value;
+                OnPropertyChanged("ProgressBarText");
+            }
+        }
         public bool? IsDeleteAllCheckBoxChecked
         {
             get => (_isDeleteAllCheckBoxChecked != null) ? _isDeleteAllCheckBoxChecked : false;
@@ -75,6 +85,7 @@ namespace OPTEL.UI.Desktop.ViewModels
             _errorsListWindowService = errorsListWindowService;
             IsImportingData = false;
             IsDeleteAllCheckBoxChecked = false;
+            ProgressBarText = LocalizationManager.Instance.GetValue("Window.ImportExcel.ImportingData");
         }
 
         #region Commands
@@ -98,7 +109,6 @@ namespace OPTEL.UI.Desktop.ViewModels
             {
                 return _beginExcelFileImportCommand ??= new RelayCommand(async obj =>
                 {
-                    IsImportingData = true;
                     _windowCloseService.SetAllowWindowClosing(false);
                     _windowCloseService.SetReasonMessage(LocalizationManager.Instance.GetValue("Window.ImportExcel.Errors.ClosingWhileImporting"));
                     if (SelectedExcelFile == null)
@@ -111,6 +121,37 @@ namespace OPTEL.UI.Desktop.ViewModels
                             MessageBoxButton.OK,
                             MessageBoxImage.Error);
                         return;
+                    }
+                    if (IsDeleteAllCheckBoxChecked == true)
+                    {
+                        MessageBoxResult result = MessageBox.Show(
+                            LocalizationManager.Instance.GetValue("Window.ImportExcel.Warnings.DeleteAllDataFromDatabase"),
+                            LocalizationManager.Instance.GetValue("Window.Global.MessageBox.Warning.Global.Title"),
+                            MessageBoxButton.YesNo,
+                            MessageBoxImage.Warning);
+                        if (result == MessageBoxResult.No)
+                        {
+                            return;
+                        }
+                        IsImportingData = true;
+                        ProgressBarText = LocalizationManager.Instance.GetValue("Window.ImportExcel.DeletingData");
+                        await Task.Run(() =>
+                        {
+                            Database.instance.CalibrationChangeRepository.DeleteRange(Database.instance.CalibrationChangeRepository.GetAll());
+                            Database.instance.CoolingLipChangeRepository.DeleteRange(Database.instance.CoolingLipChangeRepository.GetAll());
+                            Database.instance.CustomerRepository.DeleteRange(Database.instance.CustomerRepository.GetAll());
+                            Database.instance.FilmRecipeChangeRepository.DeleteRange(Database.instance.FilmRecipeChangeRepository.GetAll());
+                            Database.instance.FilmRecipeRepository.DeleteRange(Database.instance.FilmRecipeRepository.GetAll());
+                            Database.instance.FilmTypeRepository.DeleteRange(Database.instance.FilmTypeRepository.GetAll());
+                            Database.instance.NozzleChangeRepository.DeleteRange(Database.instance.NozzleChangeRepository.GetAll());
+                            Database.instance.OrderRepository.DeleteRange(Database.instance.OrderRepository.GetAll());
+                            Database.instance.ProductionLineRepository.DeleteRange(Database.instance.ProductionLineRepository.GetAll());
+                            Database.instance.Save();
+                        });
+                        ProgressBarText = LocalizationManager.Instance.GetValue("Window.ImportExcel.ImportingData");
+                    } else
+                    {
+                        IsImportingData = true;
                     }
                     bool error = false;
                     ObservableCollection<Error> errors = new ObservableCollection<Error>();
@@ -137,7 +178,7 @@ namespace OPTEL.UI.Desktop.ViewModels
                                 }
                             });
                     }
-                    catch(Exception ex)
+                    catch (Exception ex)
                     {
                         currentException = ex;
                         while (currentException != null)
