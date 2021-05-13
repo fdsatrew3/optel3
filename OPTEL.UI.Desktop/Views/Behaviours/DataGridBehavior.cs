096dcea8f82a9185ac316ac1f4006e561430c910
fdsatrew <fdsatrewq3@gmail.com>
2021-05-13 14:18:19 +0300

    Refactoring

--- OPTEL.UI.Desktop/Views/Behaviours/DataGridBehavior.cs
+++ OPTEL.UI.Desktop/Views/Behaviours/DataGridBehavior.cs
@@ -5,7 +5,7 @@ using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Media;
 
-namespace OPTEL.UI.Desktop.Behaviours
+namespace OPTEL.UI.Desktop.Views.Behaviours
 {
     public class DataGridBehavior
     {
