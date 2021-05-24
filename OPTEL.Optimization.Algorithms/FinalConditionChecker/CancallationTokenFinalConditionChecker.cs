adb0f99197bef96da15fd0c797c0516b7eeeeb61
Александр Плеханов <Plehalex09@gmail.com>
2021-05-24 10:24:19 +0300

    [OPT-2][OPT-29] - CancallationTokenFinalConditionChecker was added

---
+++ OPTEL.Optimization.Algorithms/FinalConditionChecker/CancallationTokenFinalConditionChecker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using Optimization.Algorithms;
+
+namespace OPTEL.Optimization.Algorithms.FinalConditionChecker
+{
+    public class CancallationTokenFinalConditionChecker<T> : IFinalCoditionChecker<T>
+    {
+        private readonly CancellationTokenSource _cancellationTokenSource;
+        
+        private CancellationToken CancellationToken => _cancellationTokenSource.Token;
+
+        public CancallationTokenFinalConditionChecker(CancellationTokenSource cancellationTokenSource)
+        {
+            _cancellationTokenSource = cancellationTokenSource ?? throw new ArgumentNullException(nameof(cancellationTokenSource));
+        }
+
+        public void Begin()
+        {
+
+        }
+
+        public bool IsStateFinal(T state)
+        {
+            return CancellationToken.IsCancellationRequested;
+        }
+    }
+}
