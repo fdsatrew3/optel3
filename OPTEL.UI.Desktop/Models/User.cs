71d409af2a947d69b3a1689cf7b67a8e0fb2a16a
fdsatrew <fdsatrewq3@gmail.com>
2021-06-11 19:22:28 +0300

    Added missing file ._.

---
+++ OPTEL.UI.Desktop/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using OPTEL.UI.Desktop.Models.Base;
+
+namespace OPTEL.UI.Desktop.Models
+{
+    public class User : IDataModel
+    {
+        public enum Types
+        {
+            Adminstrator,
+            KnowledgeEngineer,
+            ProductionDirector
+        }
+        public Types Type { get; set; }
+
+        public Data.Users.User DataUser { get; set; }
+
+        public User(Data.Users.User user)
+        {
+            DataUser = user;
+        }
+    }
+}
